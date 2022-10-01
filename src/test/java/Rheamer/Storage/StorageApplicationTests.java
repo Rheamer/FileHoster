d7f412984d51d83f99de5ffc133e8fce5a37a1a1
Rheamer <step.rheamer@gmail.com>
2022-10-01 16:02:15 +0300

    tests

--- src/test/java/Rheamer/Storage/StorageApplicationTests.java
+++ src/test/java/Rheamer/Storage/StorageApplicationTests.java
@@ -1,13 +1,30 @@
 package Rheamer.Storage;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class StorageApplicationTests {
 
+	@Autowired
+	private RequestRepository requestRepository;
+
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
+	public void uploadPicture() throws Exception {
+		var request = MockMvcRequestBuilders.post("/uploadPhoto");
+		request.content("123123123");
+		ResultActions response = mockMvc.perform(request);
+		response.andExpect(MockMvcResultMatchers.status().isAccepted());
 	}
 
 }
