a9b06eca5ea4c3e71333f8f97f291cf03f3c4e7b
Rheamer <step.rheamer@gmail.com>
2022-10-01 21:59:51 +0300

    kafka tests with kafkaembeddedbroker

--- src/test/java/Rheamer/Storage/StorageApplicationTests.java
+++ src/test/java/Rheamer/Storage/StorageApplicationTests.java
@@ -1,30 +1,107 @@
 package Rheamer.Storage;
 
+import kafka.controller.KafkaController;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.aspectj.lang.annotation.Before;
+import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.KafkaListenerEndpointRegistry;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.listener.MessageListenerContainer;
+import org.springframework.kafka.test.EmbeddedKafkaBroker;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.kafka.test.utils.ContainerTestUtils;
+import org.springframework.kafka.test.utils.KafkaTestUtils;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.nio.charset.StandardCharsets;
+
+@EnableKafka
 @SpringBootTest
 @AutoConfigureMockMvc
+@EmbeddedKafka(partitions = 1, brokerProperties = { "listeners=PLAINTEXT://localhost:9092", "port=9092" })
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class StorageApplicationTests {
+
+	@Autowired
+	EmbeddedKafkaBroker kafkaEmbedded;
+
+	@Autowired
+	KafkaListenerEndpointRegistry kafkaListenerEndpointRegistry;
+
+	@Autowired
+	private KafkaTemplate<String, Photo> kafkaTemplate;
 
 	@Autowired
 	private RequestRepository requestRepository;
 
 	@Autowired
+	KafkaPhotoUploadService kfProducer;
+	@Autowired
+	private KafkaConsumer consumer;
+
+	@Autowired
 	private MockMvc mockMvc;
+
+	@BeforeAll
+	public void setUp() throws Exception {
+		for (MessageListenerContainer messageListenerContainer : kafkaListenerEndpointRegistry.getListenerContainers()) {
+			ContainerTestUtils.waitForAssignment(messageListenerContainer,
+					kafkaEmbedded.getPartitionsPerTopic());
+		}
+	}
 
 	@Test
 	public void uploadPicture() throws Exception {
 		var request = MockMvcRequestBuilders.post("/uploadPhoto");
-		request.content("123123123");
+		JSONObject jo = new JSONObject();
+		jo.put("name", "mydad");
+		jo.put("data", "mydad".getBytes(StandardCharsets.UTF_8));
 		ResultActions response = mockMvc.perform(request);
 		response.andExpect(MockMvcResultMatchers.status().isAccepted());
 	}
 
+	@Test
+	public void uploadPictureVerbose() throws Exception {
+		var request = MockMvcRequestBuilders.post("/uploadPhoto");
+		var photoDto = new PhotoDto("mydad", "mydad".getBytes(StandardCharsets.UTF_8));
+		DtoMapperPhoto.validate(photoDto);
+		var photo = DtoMapperPhoto.toPhoto(photoDto);
+		kfProducer.sendPhoto(photo);
+
+	}
+
+	@TestConfiguration
+	public static class TestConfig {
+		@Value(value = "${spring.kafka.bootstrap-servers}")
+		private String bootstrapAddress;
+		@Autowired
+		EmbeddedKafkaBroker kafkaBroker;
+		@Bean
+		public ProducerFactory<String, String> producerFactory() {
+			var props = KafkaTestUtils.producerProps(kafkaBroker);
+			return new DefaultKafkaProducerFactory<>(props);
+		}
+
+		@Bean
+		public KafkaTemplate<String, String> kafkaTemplate() {
+			KafkaTemplate<String, String> kafkaTemplate = new KafkaTemplate<>(producerFactory());
+			return kafkaTemplate;
+		}
+	}
 }
