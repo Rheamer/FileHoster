5267982ca4b154b50d158a928793a8c69689ba59
Rheamer <step.rheamer@gmail.com>
2022-10-07 11:32:46 +0300

    switch from validation mapper to valid annotation

--- src/main/java/Rheamer/Storage/PhotoDto.java
+++ src/main/java/Rheamer/Storage/PhotoDto.java
@@ -3,9 +3,13 @@ package Rheamer.Storage;
 import lombok.Builder;
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 @Builder
 public class PhotoDto {
+    @NotBlank
     private final String name;
+    @NotBlank
     private final byte[] data;
 }
--- src/main/java/Rheamer/Storage/RestfulController.java
+++ src/main/java/Rheamer/Storage/RestfulController.java
@@ -4,16 +4,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+
 @RestController
 @RequestMapping(produces = "application/json")
 public class RestfulController {
-    @Autowired
     KafkaPhotoUploadService messageQueue;
+
+    @Autowired
+    public RestfulController(KafkaPhotoUploadService messageQueue) {
+        this.messageQueue = messageQueue;
+    }
 
     @ResponseStatus(HttpStatus.ACCEPTED)
     @PostMapping("/uploadPhoto")
-    public void uploadFile(@RequestBody PhotoDto photoDto){
-        DtoMapperPhoto.validate(photoDto);
+    public void uploadFile(@Valid @RequestBody PhotoDto photoDto){
         var photo = DtoMapperPhoto.toPhoto(photoDto);
         messageQueue.sendPhoto(photo);
     }
